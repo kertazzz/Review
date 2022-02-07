28e2da64327165aadbd562ccec2188007c16ec91
Alex <kertazzz@gmail.com>
2022-02-07 20:59:33 +0300

    fix

---
+++ src/Array.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Array {
+
+
+        public static void main (String[]args){
+
+            int[] arr = new int[10];
+
+            int sum = 0;
+            int countOf5 = 0;
+            int max = 0;
+            for (int i : arr) {
+                arr[i] = ThreadLocalRandom.current().nextInt(1000);
+                System.out.println(arr[i]);
+                if (arr[i] % 2 == 0) {
+                    sum += arr[i];
+                }
+                if (arr[i] == 5) {
+                    countOf5++;
+                }
+                if (arr[i] > max){
+                    max = arr[i];
+                }
+            }
+            int mediumSum = (sum / arr.length);
+
+            System.out.println("Сумма = " + sum);
+            System.out.println("Max = " + max);
+            System.out.println("Среднеарифметическое = " + mediumSum);
+
+        }
+    }
