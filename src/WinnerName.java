d47576f57c0920f498d881fc0c8b2a65abce43ce
Alex <kertazzz@gmail.com>
2022-02-09 08:15:12 +0300

    new task

---
+++ src/WinnerName.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+ class Bowling {
+    HashMap<String, Integer> players;
+    Bowling() {
+        players = new HashMap<>();
+    }
+    public void addPlayer(String name, int p) {
+        players.put(name, p);
+    }
+    public void getWinner() {
+
+        String winnerName = "";
+        int winnerScore = 0;
+        for (Map.Entry<String, Integer> entry: players.entrySet()){
+              int points = entry.getValue();
+              String name = String.valueOf(entry.getKey());
+              if(winnerScore < points){
+                  winnerScore = points;
+                  winnerName = name;
+              }
+            }
+        System.out.println(winnerName);
+        }
+    }
+
+public class WinnerName {
+    public static void main(String[ ] args) {
+        Bowling game = new Bowling();
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0;i<3;i++) {
+            String input = sc.nextLine();
+            String[] values = input.split(" ");
+            String name = values[0];
+            int points = Integer.parseInt(values[1]);
+            game.addPlayer(name, points);
+        }
+
+        game.getWinner();
+    }
+}
